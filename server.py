254984a080e3fceb4177913f8ea70b22ff3149fc
Reality <Ighoafokereality1@gmail.com>
2025-03-27 07:33:30 +0100

    use print statement to debugg why recv&all function is returning none

--- server.py
+++ server.py
@@ -76,19 +76,21 @@ def handle_clients(client_sock,addr):
 
 def reciev_full_data(sock):
     try:
+        print("about to receiving data")
         headers=''
         while "\r\n\r\n" not in headers:
             chunk=sock.recv(1024).decode()
             if not chunk:
                 return None
             headers+=chunk
+        print("head received")
         if headers.startswith('OPTIONS'):
             sock.send(preflight_headers.encode("utf-8"))
             sock.close()
             return
         header,remaining=headers.split('\r\n\r\n',1)
         content_length=0
-        print("HEADER PRINT===>",header)
+        print("HEADER PRINT===>",repr(header))
         for line in header.split('\r\n'):
             if line.lower().startswith('content-length:'):
                 content_length=line.split(':')[1].strip()
@@ -101,6 +103,7 @@ def reciev_full_data(sock):
                 break
             remaining_byte=int(content_length)-len(body)
             body+=chunk[:remaining_byte]
+        print("body",repr(body))
         response=header + '\r\n\r\n'+ body
         return response
     except Exception as error:
